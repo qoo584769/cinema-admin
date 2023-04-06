00e240f0547df917bc413fe4d58bea22481b8076
JasonLiu104 <jasonliu104com@gmail.com>
2023-04-06 16:45:42 +0800

    [設定]init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+export const store = createStore({
+  state () {
+    return {
+      count: 1
+    }
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+
+  }
+})
