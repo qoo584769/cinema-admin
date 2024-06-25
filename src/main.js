ebbcbf19891a527cf47ea2db02e4d08ec0dcdfbd
ming wu <a85062@pchome.com.tw>
2024-06-25 22:55:30 +0800

    【修改】路徑問題

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import { stores } from './stores'
 import router from './router'
-import globalDirectives from '../src/directives'
-import globalErrorPlugin from '../src/plugins/globalErrorPlugin'
+import globalDirectives from './directives'
+import globalErrorPlugin from './plugins/globalErrorPlugin'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import '../src/assets/scss/main.scss'
+import './assets/scss/main.scss'
 
 const app = createApp(App)
 
