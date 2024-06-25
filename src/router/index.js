5ecc0d355a8b018c168bdd50a9d74b6823d126db
ming wu <a85062@pchome.com.tw>
2024-06-25 23:10:10 +0800

    【修改】路由模式問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import {
-  createRouter, createWebHistory
+  createRouter, createWebHashHistory
 } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import AdminView from '@/views/AdminView.vue'
@@ -7,7 +7,7 @@ import DashboardView from '@/views/DashboardView.vue'
 import MovieView from '@/views/MovieView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
